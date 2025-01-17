a3c46b06717ecd3982f83157c1cdfd84b6019392
Fardin Khan <153430223+Fardin4585@users.noreply.github.com>
2025-01-17 15:03:07 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+from tkinter import Tk,Entry,Button,StringVar
+
+class Calculator:
+    def __init__(self,master):
+        master.title("Calculator")
+        master.geometry('357*420+0+0')
+
+
+root=Tk()
+root.mainloop()
